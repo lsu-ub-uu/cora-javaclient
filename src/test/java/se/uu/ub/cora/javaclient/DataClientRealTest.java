8027a7d069b102970238ff212ed1a852893ea1f2
perebartrolisimo <pere.bartroli@ub.uu.se>
2023-02-20 16:06:26 +0000

    add license

--- src/test/java/se/uu/ub/cora/javaclient/DataClientRealTest.java
+++ src/test/java/se/uu/ub/cora/javaclient/DataClientRealTest.java
@@ -1,3 +1,21 @@
+/*
+ * Copyright 2023 Uppsala University Library
+ *
+ * This file is part of Cora.
+ *
+ *     Cora is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ *
+ *     Cora is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ *
+ *     You should have received a copy of the GNU General Public License
+ *     along with Cora.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package se.uu.ub.cora.javaclient;
 
 import static org.testng.Assert.assertEquals;
@@ -80,7 +98,7 @@ public class DataClientRealTest {
 		// assertEquals(read.getReadPermissions(), "");
 	}
 
-	@Test(enabled = false)
+	@Test(enabled = true)
 	public void testProvaLite() throws Exception {
 
 		DataClient dataClient = dataClientFactory.factorUsingUserIdAndAppToken("141414",
